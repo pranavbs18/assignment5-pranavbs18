9da987f50f18b797f48f58ac6678cac06a7bfea9
pranavbs18 <109842852+pranavbs18@users.noreply.github.com>
2026-02-20 12:11:39 +0530

    Fixed package to reference previous repo

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,28 +1,39 @@
-
 ##############################################################
 #
 # AESD-ASSIGNMENTS
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_SITE = git@github.com:pranavbs18/assignment3-pranavbs18.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_VERSION = main
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+
+	# Create config directory
+	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf
+
+	# Install config files
+	$(INSTALL) -m 0644 $(@D)/finder-app/conf/* \
+		$(TARGET_DIR)/etc/finder-app/conf/
+
+	# Install writer binary
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer \
+		$(TARGET_DIR)/usr/bin/
+
+	# Install finder script
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh \
+		$(TARGET_DIR)/usr/bin/
+
+	# Install finder-test script
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh \
+		$(TARGET_DIR)/usr/bin/
+
 endef
 
 $(eval $(generic-package))
