9664a307475b66188c19331aa4451e0157a0a1e0
pranavbs18 <109842852+pranavbs18@users.noreply.github.com>
2026-02-23 15:46:19 +0530

    Finalizing S99 script

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,39 +1,14 @@
-##############################################################
-#
-# AESD-ASSIGNMENTS
-#
-##############################################################
-
-AESD_ASSIGNMENTS_SITE = git@github.com:pranavbs18/assignment3-pranavbs18.git
+AESD_ASSIGNMENTS_SITE = 'https://github.com/pranavbs18/assignment3-pranavbs18'
 AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_VERSION = 'main'
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AESD_ASSIGNMENTS_VERSION = '2a30c3c4ac7a8c41741e03f72e914afa9e2a28c5'
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-
-	# Create config directory
-	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf
-
-	# Install config files
-	$(INSTALL) -m 0644 $(@D)/finder-app/conf/* \
-		$(TARGET_DIR)/etc/finder-app/conf/
-
-	# Install writer binary
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer \
-		$(TARGET_DIR)/usr/bin/
-
-	# Install finder script
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh \
-		$(TARGET_DIR)/usr/bin/
-
-	# Install finder-test script
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh \
-		$(TARGET_DIR)/usr/bin/
-
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
